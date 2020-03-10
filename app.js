781f193547f3dd32de9cad573ec7628e481ca59d
TorrieL <hmminicity2@163.com>
2020-03-11 00:41:13 +0800

    play node

---
+++ app.js
@@ -0,0 +1,9 @@
+class User{
+  constructor(){
+    this.username = "Bender";
+    this.password = "123455"
+  }
+}
+
+const user1 = new User();
+console.log(user1);
---
+++ app1.js
@@ -0,0 +1,3 @@
+let user = require('./app')
+//node will give u a blank object when no content exported
+console.log(user); // {}
